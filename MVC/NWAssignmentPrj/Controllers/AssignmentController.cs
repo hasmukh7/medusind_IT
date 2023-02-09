da3b715d2771ff54215f40d2bc42a6cd0e384f4b
hasmukh7 <hasmukhm0808@gmail.com>
2023-02-10 01:19:06 +0530

    mvc assignment

---
+++ MVC/NWAssignmentPrj/Controllers/AssignmentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NWAssignmentPrj.Models;
+namespace NWAssignmentPrj.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        // GET: Assignment
+        NorthwindEntities db = new NorthwindEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("GermanyCutomer")]
+        public ActionResult GetGermanyCustomer()
+        {
+            return View("GetGermanyCustomer", db.GetGermanyCust().ToList());
+        }
+
+        public ActionResult GetByOrderId()
+        {
+            return View("GetByOrderId", db.Orders.Where(x => x.orderid == 10248).ToList());
+        } 
+    }
+}
